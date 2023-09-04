a9682a959f02528e38861ad7cc1ccad81e6259fe
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-09-04 06:03:05 +0200

    update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let setTimer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', randomColorStart);
+btnStop.addEventListener('click', randomColorStop);
+
+function randomColorStart() {
+    setTimer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+function randomColorStop(params) {
+    setTimer = clearInterval(setTimer);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+};
